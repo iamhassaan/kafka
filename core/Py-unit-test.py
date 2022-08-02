6fb9d83562da1e99216a1ddf1f3e80cb60db58f1
M Anwar <33649841+iamhassaan@users.noreply.github.com>
2022-08-02 22:28:13 +0100

    Create Py-unit-test.py

---
+++ core/Py-unit-test.py
@@ -0,0 +1,17 @@
+class test_data:
+    name = []
+
+    def set_name(self, user_name):
+        self.name.append(user_name)
+        return len(self.name) - 1
+
+    def get_name(self, user_id):
+        if user_id >= len(self.name):
+            return 'There is no such user'
+        else:
+            return self.name[user_id]
+
+
+if __name__ == '__main__':
+    person = Person()
+    
